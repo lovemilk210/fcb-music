bbf773c8513e65b3564ef3992a7fc13936891e48
lovemilk210 <lovemilk@gmail.com>
2022-05-11 11:47:16 +0800

    update

--- src/test/java/tw/com/fcb/fcb_music/MusicServiceTest.java
+++ src/test/java/tw/com/fcb/fcb_music/MusicServiceTest.java
@@ -11,7 +11,7 @@ class MusicServiceTest {
 		MusicService service = new MusicServiceImpl();
 		Music music = service.getByCode("A01");
 		
-		assertEquals("Jay",music.getName());
+		assertEquals("Jay2",music.getName());
 
 	}
 
